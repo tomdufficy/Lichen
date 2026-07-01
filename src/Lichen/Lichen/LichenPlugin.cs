a6a5d1e5070a7d7ebcdabf4b0b0b511f172daaa7
Tom <Tom@KRAKEN>
2026-07-01 21:28:45 +0200

    correcting error code
    user experiences error on install from package manager, this should fix that

--- src/Lichen/Lichen/LichenPlugin.cs
+++ src/Lichen/Lichen/LichenPlugin.cs
@@ -18,7 +18,16 @@ namespace Lichen
 
         protected override LoadReturnCode OnLoad(ref string errorMessage)
         {
-            string facadesFolder = Path.Combine(Path.GetDirectoryName(PlugIn.PathFromName("Lichen")), "facades");
+            string pluginPath = GetType().Assembly.Location;
+            string pluginFolder = Path.GetDirectoryName(pluginPath);
+
+            if (string.IsNullOrEmpty(pluginFolder))
+            {
+                errorMessage = "Lichen could not determine the plug-in folder.";
+                return LoadReturnCode.ErrorShowDialog;
+            }
+
+            string facadesFolder = Path.Combine(pluginFolder, "facades");
 
             Catalogue = new LichenCatalogue(facadesFolder);
             Catalogue.Load();
